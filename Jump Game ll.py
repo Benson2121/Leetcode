54c5ce12dd3ebf4c730f878b31918b1c00613326
Benson Li <81859388+Benson2121@users.noreply.github.com>
2021-08-22 09:06:03 -0400

    Create Jump Game ll.py

---
+++ Jump Game ll.py
@@ -0,0 +1,12 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        k=0
+        while len(nums)>1:
+            big=0
+            for i,num in enumerate(nums):
+                if num>=len(nums)-1-i>big:
+                    big=len(nums)-1-i
+            k=k+1
+            nums=nums[:-big]
+        
+        return k
